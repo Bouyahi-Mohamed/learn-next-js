d009391a28c9488361794d523889e8bccf0f1bca
Bouyahi Mohamed <bouyahi.mohamed.1@gmail.com>
2026-03-20 20:39:25 +0100

    learning routing nextjs scenario 2

---
+++ src/app/about/page.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <main className="container mx-auto px-4 py-8 secodary" >
+      <h1>About Us</h1>
+      <p>This is the about page of my application.</p>
+    </main>
+  );
+}
