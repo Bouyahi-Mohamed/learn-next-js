c190975b6efcdcdeca208c2650679dd584551352
Bouyahi Mohamed <bouyahi.mohamed.1@gmail.com>
2026-03-20 22:45:04 +0100

    learning routing nextjs (dynamic route) scenario 6

---
+++ src/app/doc/page.js
@@ -0,0 +1,9 @@
+export default function documentation() {
+    return (
+        <main className="container mx-auto px-4 py-8 secodary" >
+            <h1>Documentation</h1>
+            <p>This is the documentation page of my application.</p>
+
+        </main>
+    );
+}
