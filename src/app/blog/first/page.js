f9784852ea1fc66636bd17cccb1bea552d1ebe46
Bouyahi Mohamed <bouyahi.mohamed.1@gmail.com>
2026-03-20 20:44:56 +0100

    learning routing nextjs scenario 3

---
+++ src/app/blog/first/page.js
@@ -0,0 +1,8 @@
+export default function First() {
+  return (
+    <main className="container mx-auto px-4 py-8 secodary" >
+      <h1>First Blog Post</h1>
+      <p>This is the first blog post of my application.</p>
+    </main>
+  );
+}
