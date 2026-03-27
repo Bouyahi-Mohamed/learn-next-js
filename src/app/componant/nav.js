d5e180efe8d841412e9b5e6170abc23b687462e2
Bouyahi Mohamed <bouyahi.mohamed.1@gmail.com>
2026-03-27 15:32:24 +0100

    update nav component to highlight active link based on pathname

--- src/app/componant/nav.js
+++ src/app/componant/nav.js
@@ -1,24 +1,28 @@
+"use client"
 import Link from "next/link";
 import "../globals.css";
+import { usePathname } from "next/navigation";
+import { use } from "react";
 export default function Nav() {
+    const pathname = usePathname();
   return (
     <>
       <nav className="bg-gray-800 text-white p-4 space-x-4 flex flex-row justify-center">
         <ul className="flex space-x-4 justify-center">
           <li>
-            <Link href="/">Home</Link>
+            <Link href="/" className={pathname === "/" ? "font-bold mr-4" : "text-gray-500 mr-4"}>Home</Link>
           </li>
           <li>
-            <Link href="/blog">blog</Link>
+            <Link href="/blog" className={pathname === "/blog" ? "font-bold mr-4" : "text-gray-500 mr-4"}>blog</Link>
           </li>
           <li>
-            <Link href="/doc">documentation</Link>
+            <Link href="/doc" className={pathname === "/doc" ? "font-bold mr-4" : "text-gray-500 mr-4"}>documentation</Link>
           </li>
           <li>
-            <Link href="/products">Products</Link>
+            <Link href="/products" className={pathname === "/products" ? "font-bold mr-4" : "text-gray-500 mr-4"}>Products</Link>
           </li>
           <li>
-            <Link href="/about">about</Link>
+            <Link href="/about" className={pathname === "/about" ? "font-bold mr-4" : "text-gray-500 mr-4"}>about</Link>
           </li>
         </ul>
       </nav>
